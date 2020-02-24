a89b0b6a0eb815d71002de4c9250f70e0eec05f8
Nathaniel Redmon <52013906+nonstopper0@users.noreply.github.com>
2020-02-24 12:32:06 -0700

    re-did contact section

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
-function callServers()  {
-    const request = new window.XMLHttpRequest("https://fixify-react-app.herokuapp.com/")
-    const flaskrequest =  new window.XMLHttpRequest("https://fixify-flask-app.herokuapp.com/")
-    const instantgramrequest = new window.XMLHttpRequest("https://instantgrammer.herokuapp.com/home")
-    const nomirequest = new window.XMLHttpRequest("https://nomistock.herokuapp.com/")
-    console.log(request, flaskrequest, instantgramrequest, nomirequest)
+var urls = ["https://fixify-react-app.herokuapp.com", "https://nomistock.herokuapp.com/", "https://instantgrammer.herokuapp.com/home", "https://nomistockexpress.herokuapp.com/"];
+for (var i = 0; i < urls.length; i++) {
+  $.ajaxPrefilter(function (options) {
+    if (options.crossDomain && jQuery.support.cors) {
+      var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
+      options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
+    }
+  });
+  $.get(
+    urls[i],
+    function (response) {
+    });
 }
-callServers()
-setInterval(() => {callServers()}, 100000)
